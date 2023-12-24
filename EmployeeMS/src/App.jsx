82608b6678a51a0bc23fe5044b734a8d54d6499b
saduni9 <rasikalahms.20@uom.lk>
2023-12-24 14:56:19 +0530

    category part completed

--- EmployeeMS/src/App.jsx
+++ EmployeeMS/src/App.jsx
@@ -3,6 +3,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Login from './Components/Login';
 import{BrowserRouter, Routes,Route} from 'react-router-dom'
 import Dashboard from './Components/Dashboard';
+import Home from './Components/Home';
+import Employee from './Components/Employee';
+import Category from './Components/Category';
+import Profile from './Components/Profile';
+import AddCategory from './Components/AddCategory';
 
 function App() {
   
@@ -11,7 +16,13 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/adminlogin' element={<Login/>}></Route>
-      <Route path='/dashboard' element={<Dashboard/>}></Route>
+      <Route path='/dashboard' element={<Dashboard/>}>
+        <Route path='' element={<Home/>}></Route>
+        <Route path='/dashboard/employee' element={<Employee />}></Route>
+        <Route path='/dashboard/category' element={<Category/>}></Route>
+        <Route path='/dashboard/profile' element={<Profile/>}></Route>
+        <Route path='/dashboard/add_category' element={<AddCategory/>}></Route>
+      </Route>
     </Routes>
     </BrowserRouter>
   )
--- Server/Routes/AdminRoute.js
+++ Server/Routes/AdminRoute.js
@@ -8,7 +8,7 @@ router.post("/adminlogin", (req, res) => {
   const sql = "SELECT * from admin where email = ? and password = ?";
   con.query(sql, [req.body.email, req.body.password], (err, result) => {
     if (err)
-      return res.json({ loginStatus: false, Error: "Wrong email or password" });
+      return res.json({ loginStatus: false, Error: "Query error" });
     if (result.length > 0) {
       const email = result[0].email;
       const token = jwt.sign(
@@ -23,5 +23,22 @@ router.post("/adminlogin", (req, res) => {
     }
   });
 });
+
+router.get('/category', (req,res) => {
+  const sql = "SELECT * FROM category";
+  con.query(sql,(err,result) =>{
+    if(err) return res.json({Status: false, Error:"Query Error"})
+    return res.json({Status: true,Result: result})
+  })
+})
+
+router.post('/add_category', (req, res) =>{
+  const sql = "INSERT INTO category (`name`)VALUES (?)"
+  con.query(sql, [req.body.category],(err,result) => {
+    if(err) return res.json({Status: false, Error:"Query Error"})
+    return res.json({Status: true})
+  })
+})
+
 
 export { router as adminRouter };
