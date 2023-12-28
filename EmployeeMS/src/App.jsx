4019dabd45082240c4c846f917ac550bfc05b01d
saduni9 <rasikalahms.20@uom.lk>
2023-12-28 12:28:51 +0530

    Ëmployee details part completed

--- EmployeeMS/src/App.jsx
+++ EmployeeMS/src/App.jsx
@@ -10,6 +10,9 @@ import Profile from './Components/Profile';
 import AddCategory from './Components/AddCategory';
 import AddEmployee from './Components/AddEmployee';
 import EditEmployee from './Components/EditEmployee';
+import Start from './Components/Start';
+import EmployeeLogin from './Components/EmployeeLogin';
+import EmployeeDetails from './Components/EmployeeDetails';
 
 function App() {
   
@@ -17,15 +20,19 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path='/employee_login' element={<EmployeeLogin/>}></Route>
+      <Route path='/start' element={<Start/>}></Route>
       <Route path='/adminlogin' element={<Login/>}></Route>
+      <Route path='/employee_details/:id' element={<EmployeeDetails/>}></Route>
       <Route path='/dashboard' element={<Dashboard/>}>
-        <Route path='' element={<Home/>}></Route>
+      <Route path='' element={<Home/>}></Route>
         <Route path='/dashboard/employee' element={<Employee />}></Route>
         <Route path='/dashboard/category' element={<Category/>}></Route>
         <Route path='/dashboard/profile' element={<Profile/>}></Route>
         <Route path='/dashboard/add_category' element={<AddCategory/>}></Route>
         <Route path='/dashboard/add_employee' element={<AddEmployee/>}></Route>
         <Route path='/dashboard/edit_employee/:id' element={<EditEmployee/>}></Route>
+        
       </Route>
     </Routes>
     </BrowserRouter>
--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from 'cors'
 import { adminRouter } from "./Routes/AdminRoute.js";
+import { EmployeeRouter } from "./Routes/EmployeeRoute.js";
 
 const app = express()
 app.use(cors({
@@ -10,6 +11,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use('/auth', adminRouter)
+app.use('/employee', EmployeeRouter)
 app.use(express.static('Public'))
 
 app.listen(3000, () => {
